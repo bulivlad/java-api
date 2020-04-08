90b696026bd0037ae45cfc2a047549fc5093ce12
Vlad Bulimac <vbulimac@deloittece.com>
2020-04-08 22:41:16 +0300

    removes the transactionId from RefundData as the API was returning 400 Bad Request & bumps version to 0.9.4

--- src/main/java/com/paylike/models/requests/CreateTransactionFromPreviousTransactionData.java
+++ src/main/java/com/paylike/models/requests/CreateTransactionFromPreviousTransactionData.java
@@ -3,17 +3,23 @@ package com.paylike.models.requests;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
+import java.util.Map;
+
 /**
  * Created by jankjr on 17/11/2016.
  * Contributor Vlad Bulimac on 05/04/2020.
  */
 
 @Data
+@Builder
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class RefundData {
+public class CreateTransactionFromPreviousTransactionData {
+  String transactionId;
   String descriptor;
+  String currency;
   long amount;
+  Map custom;
 }
